8c7c1e237717dd729acde9591b568afc5693b564
mhudecheck <mhudecheck@gmail.com>
2017-05-04 15:11:41 +0200

    Add files via upload

--- R/revgeo.R
+++ R/revgeo.R
@@ -1,6 +1,6 @@
-# This code enables reverse geocoding with Google Maps API and Photon API  
+# This code enables reverse geocoding with Google Maps API, Bing API, and Photon API  
 # revgeo() requires RCurl and RJSONIO to function properly
-# You must provide a valid Google Maps API to reverse geocode with Google Maps
+# You must provide a valid Google Maps API to reverse geocode with Google Maps.  Same with Bing. 
 # You can specify whether you want the output to be a string, a hashed string, or a dataframe with option="string" | "hash" | "frame"
 # If you specify an option that isn't included in the list, the function will return the results as a dataframe
 # Specifying a hashed string or dataframe allows you to specify whether you want to return the entire results, or a single item with item=""
@@ -9,13 +9,13 @@
 # This package is licensed under GPL 3.0
 # Please contact Mike Hudecheck at michael.hudecheck@gess.ethz.ch if you have any questions
 
-#' Reverse Geocoding with the Photon Geocoder for OpenStreetMap and Google Maps.
-#' @description Enables the use of the Photon geocoder for OpenStreetMap and Google Maps to reverse geocode coordinate pairs. Photon allows for unlimited geocode queries, while Google Maps provides a little more information for 'out of the way' locations. Google Maps requires an API key and limits users to 2,500 free queries a day.  
+#' Reverse Geocoding with the Photon Geocoder for OpenStreetMap, Google Maps, and Bing.
+#' @description Enables the use of the Photon geocoder for OpenStreetMap, Google Maps, and Bing to reverse geocode coordinate pairs. Photon allows for unlimited geocode queries, while Google Maps and Bing provide a little more information for 'out of the way' locations. Google Maps and Bing require an API key, and Google Maps limits users to 2,500 free queries a day.  
 #' @author Michael Hudecheck, \email{michael.hudecheck@gess.ethz.ch}
 #' @param longitude Required. You must enter a valid longitude coordinate;  e.g., -77.0229529
 #' @param latitude Required. You must enter a valid latitude coordinate; e.g., 38.89283435
-#' @param google Defaults to NULL, which automatically selects the Photon API. Enter TRUE to use the Google Maps API.
-#' @param API Defaults to NULL. Enter a valid Google Maps API key to use the Google Maps API. 
+#' @param provider Defaults to NULL, which automatically selects the Photon API. Enter 'google' to use the Google Maps API or 'bing' to use the Bing API.
+#' @param API Defaults to NULL. Enter a valid Google Maps or Bing API key to use their service. 
 #' @param output Defaults to NULL, which returns a reverse geocoded address as a string.  Other valid options include 'hash', which returns a hashed string, and 'frame', which returns a dataframe.
 #' @param item Defaults to NULL. You can use 'item' in conjunction with 'hash' or 'frame' to return portion of the address; e.g., 'zip' for postal code. Options include 'housenumber', 'street', 'city', 'county', 'state', and 'country'.
 #' @examples 
@@ -29,9 +29,9 @@
 #' @import RJSONIO
 #' @export
 
-revgeo <- function(longitude, latitude, google = NULL, API = NULL, output = NULL, item = NULL) {
-  if(missing(google)) {
-    google <- NULL
+revgeo <- function(longitude, latitude, provider = NULL, API = NULL, output = NULL, item = NULL) {
+  if(missing(provider)) {
+    provider <- NULL
   }
   if(missing(API)) {
     API <- NULL
@@ -44,12 +44,12 @@ revgeo <- function(longitude, latitude, google = NULL, API = NULL, output = NULL
   }
   geocode_data <- list()
   geocode_frame <- data.frame()
-  if(is.null(google)) {
+  if(is.null(provider) || (provider %in% "photon")) {
     url <- paste0("http://photon.komoot.de/reverse?lon=", longitude,"&lat=", latitude)
     for(i in url) {
-      print(paste0("Getting geocode data from Photon:", i))
+      print(paste0("Getting geocode data from Photon: ", i))
       data <- getURLAsynchronous(i)
-      returned_data <- fromJSON(data)
+      returned_data <- tryCatch(fromJSON(data), error=function(e) "There was an issue retrieving an address from Photon.  Please check that your coordinates are coorect and try again.")
       housenumber <- tryCatch(returned_data$features[[1]]$properties$housenumber, error=function(e) "House Number Not Found") 
       street <- tryCatch(returned_data$features[[1]]$properties$street, error=function(e) "Street Not Found")
       city <- tryCatch(returned_data$features[[1]]$properties$city, error=function(e) "City Not Found") 
@@ -95,17 +95,79 @@ revgeo <- function(longitude, latitude, google = NULL, API = NULL, output = NULL
         geocode_frame <- rbind(geocode_frame, as.data.frame(geocode_data))
       }
     }
+  ### Start Bing ###
+  } else if (is.null(API) && provider %in% "Bing") {
+    print("Please enter your Bing api")
+    return()
+  } else if(provider %in% "bing") {
+    url <- paste0("http://dev.virtualearth.net/REST/v1/Locations/",latitude,",",longitude,"?o=json&key=",API)
+    if(!(is.null(item))) {
+      if(item %in% 'housenumber') {
+        print('Defaulting to street, since Bing returns house numbers and streets together.')
+        item <- 'street'
+      }
+    }
+    for(i in url) {
+      print(paste0("Getting geocode data from Bing: ", i))
+      data <- getURLAsynchronous(i)
+      returned_data <- tryCatch(fromJSON(data), error=function(e) "There was an issue retrieving an address from Bing.  Please check that your coordinates are coorect and try again.")
+      address_hash <- tryCatch(as.list(returned_data$resourceSets[[1]]$resources[[1]]$address), error=function(e) "House Number Not Found") 
+      street <- tryCatch(address_hash$addressLine, error=function(e) "House Number and Street Not Found") 
+      city <- tryCatch(address_hash$locality, error=function(e) "City Not Found")
+      state <- tryCatch(address_hash$adminDistrict, error=function(e) "State Not Found")
+      zip <- tryCatch(address_hash$postalCode, error=function(e) "Postcode Not Found")
+      country <- tryCatch(address_hash$countryRegion, error=function(e) "Country Not Found")
+      address_string <- tryCatch(address_hash$formattedAddress, error=function(e) "Address Number Not Found")
+      if(is.null(street)) {
+        street <- "House Number and Street Not Found"
+      }
+      if(is.null(city)) {
+        city <- "City Not Found"
+      }
+      if(is.null(zip)) {
+        zip <- "Postcode Not Found"
+      }
+      if(is.null(state)) {
+        state <- "State Not Found"
+      }
+      if(is.null(country)) {
+        country <- "Country Not Found"
+      }
+      if(is.null(output)) {
+        geocode_data <- append(geocode_data, address_string)
+      } else if (output == "string") {
+        geocode_data <- append(geocode_data, address_string)
+      } else if (output == "hash") {
+        geocode_data[["street"]] <- c(geocode_data[["street"]], street)
+        geocode_data[["city"]] <- c(geocode_data[["city"]], city)
+        geocode_data[["state"]] <- c(geocode_data[["state"]], state)
+        geocode_data[["zip"]] <- c(geocode_data[["zip"]], zip)
+        geocode_data[["country"]] <- c(geocode_data[["country"]], country)
+      } else {
+        geocode_data[["street"]] <- c(geocode_data[["street"]], street)
+        geocode_data[["city"]] <- c(geocode_data[["city"]], city)
+        geocode_data[["state"]] <- c(geocode_data[["state"]], state)
+        geocode_data[["zip"]] <- c(geocode_data[["zip"]], zip)
+        geocode_data[["country"]] <- c(geocode_data[["country"]], country)
+        geocode_frame <- rbind(geocode_frame, as.data.frame(geocode_data))
+      }
+    }    
+    
   ### Start Google ###
-  } else if (is.null(API) && !(is.null(google))) {
+  } else if (is.null(API) && provider %in% "google") {
       print("Please enter your Google api")
       return()
-  } else {
+  } else if(provider %in% "google") {
     url <- paste0("https://maps.googleapis.com/maps/api/geocode/json?latlng=",latitude,",",longitude,"&key=",API)
     postcode <- list()
     for(i in url) {
-      print(paste0("Getting geocode data from Google:", i))
+      print(paste0("Getting geocode data from Google: ", i))
       data <- getURLAsynchronous(i)
-      returned_data <- fromJSON(data)
+      returned_data <- tryCatch(fromJSON(data), error=function(e) "There was an issue retrieving an address from Google Maps.  Please check that your coordinates are coorect and try again.")
+      if(returned_data$status %in% "REQUEST_DENIED") {
+        print('There was an error accessing Google Maps.  Check your API key and try again.')
+        return()
+      }
       address <- returned_data$results[[1]]$formatted_address
       k <- 1
       while(k<8) {
@@ -168,6 +230,8 @@ revgeo <- function(longitude, latitude, google = NULL, API = NULL, output = NULL
         geocode_frame <- rbind(geocode_frame, as.data.frame(geocode_data))
       }
     }
+  } else {
+    print('Please enter a provider (photon, google, or bing)')
   }
   if(!nrow(geocode_frame)) {
     if(!(is.null(output %in% 'hash'))) {
